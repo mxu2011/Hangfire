b5aa13a4d4c88e4fbb5ed5bc80e65f68a0c2274d
Martin Xu <mxu2008@gmail.com>
2020-03-28 18:23:37 -0700

    added OWIN startup.cs and AuthorizationFilter.cs

---
+++ MxuDashboardSite/AuthorizationFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Hangfire.Dashboard;
+using Microsoft.Owin;
+
+namespace MxuDashboardSite
+{
+    public class AuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            // In case you need an OWIN context, use the next line, `OwinContext` class
+            // is the part of the `Microsoft.Owin` package.
+            var owinContext = new OwinContext(context.GetOwinEnvironment());
+
+            // Allow all authenticated users to see the Dashboard (potentially dangerous).
+            return true; // owinContext.Authentication.User.Identity.IsAuthenticated;
+        }
+    }
+}
---
+++ MxuDashboardSite/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.IO;
+using Hangfire;
+using Hangfire.SqlServer;
+using System.Collections.Generic;
+using Hangfire.Dashboard;
+
+namespace MxuDashboardSite
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                Authorization = new[] { new AuthorizationFilter() }
+            }, new SqlServerStorage("HangfireConnectionString"));
+        }
+    }
+}
